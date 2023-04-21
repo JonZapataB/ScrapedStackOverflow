fa004ce932860e14ebec915a1edf0e8e6b4bd005
Jon <jonzapatab@gmail.comgit config --global user.name JonZapataB>
2023-04-21 10:18:04 +0200

    enseñando todo en localhost

--- index.js
+++ index.js
@@ -10,8 +10,16 @@ app.get('/', async (req, res) => {
         <link rel="stylesheet" type="text/css" href="https://cdn.sstatic.net/Shared/stacks.css?v=83d4b324173a">
         <link rel="stylesheet" type="text/css" href="https://cdn.sstatic.net/Sites/stackoverflow/primary.css?v=5e2d45054eda">
         <h1>${title}</h1>
+        <h2>${post.fecha}</h2>
+        <h2>${post.puntuacion}</h2>
         <div>${post.post}</div>
+        <br>
+        <h2>${answer[0].fecha}</h2>
+        <h2>${answer[0].puntuacion}</h2>
         <div>${answer[0].answer}</div>
+        <br>
+        <h2>${answer[1].fecha}</h2>
+        <h2>${answer[1].puntuacion}</h2>
         <div>${answer[1].answer}</div>
 
         
